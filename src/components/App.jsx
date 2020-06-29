cff265b582f23626f3b34336b59407c439ab10e8
ShadowWalker98 <44528434+ShadowWalker98@users.noreply.github.com>
2020-06-29 11:16:31 +0530

    fetched people data as well. Added option to go back and navigate between movies and people

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,9 @@ class App extends Component {
         super();
         this.state = {
             itemList: [],
+            peopleList: [],
             displayMovies: false,
+            displayPeople: false,
         };
     }
 
@@ -30,23 +32,80 @@ class App extends Component {
             });
             //console.log(this.state.itemList);
         });
+        fetch("https://ghibliapi.herokuapp.com/people")
+        .then((res) => res.json())
+        .then((res) => {
+            let listOfPeople = [];
+            for(let i = 0; i < res.length; i++) {
+                let personDetails = {
+                    name: res[i].name,
+                    age: res[i].age,
+                }
+                listOfPeople.push(personDetails);
+            }
+            this.setState({
+                peopleList: listOfPeople,
+            });
+            console.log(this.state.peopleList);
+        });
+
+
         
     }
     
-    handleClick = () => {
-        let newState = !this.state.displayMovies
+    handleMovieClick = () => {
+        let newState = !this.state.displayMovies;
         this.setState({
-            displayMovies: newState
+            displayMovies: newState,
         });
     }
 
+    handlePeopleClick = () => {
+        let newState = !this.state.displayPeople;
+        this.setState({
+            displayPeople: newState,
+        });
+    }
+
+    goBackPeople = () => {
+        let newState = !this.state.displayPeople;
+        this.setState({
+            displayPeople: newState,
+        });
+    }
+
+    goBackMovies = () => {
+        let newState = !this.state.displayMovies;
+        this.setState({
+            displayMovies: newState,
+        });
+    }
     render() {
         
         //console.log(this.state.itemList);
+        if(this.state.displayPeople === true) {
+            return(
+                <div>
+                    <button type="button" className="btn btn-primary" onClick = {this.goBackPeople}>Go back</button>
+                    {this.state.peopleList.map((item,index) => (
+                        <div key={index} className="card" style={{width: 18 + "rem"}} >
+                            <div className="card-body">
+                                <h5 className="card-title">{item.name}</h5>
+                                <p className="card-text">{item.age}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+        
+            );
+
+        }
+
+
         if(this.state.displayMovies === true) {
             return(
                 <div>
-                    
+                    <button type="button" className="btn btn-primary" onClick = {this.goBackMovies}>Go back</button>
                     {this.state.itemList.map((item,index) => (
                         <div key={index} className="card" style={{width: 18 + "rem"}} >
                             <div className="card-body">
@@ -62,7 +121,8 @@ class App extends Component {
             return(
                 <>
                     <h3> API test</h3>
-                    <button type="button" className="btn btn-primary" onClick = {this.handleClick}>Load Movies</button>
+                    <button type="button" className="btn btn-primary" onClick = {this.handleMovieClick}>Load Movies</button>
+                    <button type="button" className="btn btn-primary" onClick = {this.handlePeopleClick}>Load People</button>
                 </>
             );
         }
